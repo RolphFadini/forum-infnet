515c2dfb1c15b7aaa475f43e80d2bd18fd434242
Rolph Fadini <rolphfadini@hotmail.com>
2024-04-17 16:28:56 -0300

    adding the home page to the app

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header/Header";
+
+const Home = () => {
+	return (
+		<div>
+			<Header />
+			<h2>Home</h2>
+			<p>Bem-vindo à página inicial!</p>
+		</div>
+	);
+};
+
+export default Home;
